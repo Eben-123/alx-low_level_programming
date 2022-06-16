41e94ba143d4fae8a986df4d8e3522de197f2bbc
Eben-123 <ebenezeretsiwah@gmail.com>
2022-06-16 08:31:20 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,9 +8,9 @@
  *
  * Return: pointer to memory area s
  */
-char *_strcat(char *s, char b, unsigned int n)
+char *_strcat(char *dest, char *src)
 {
-	unsigned int i;
+	char i;
 
 	for (i = 0; i < n; i++)
 	{
