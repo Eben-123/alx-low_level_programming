8eebbd76f9eb8b3e2ae109c10f13fa1affcf5e2f
Eben-123 <ebenezeretsiwah@gmail.com>
2022-06-24 14:43:08 -0700

    argc, argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - multiplies two numbers
+ * main - prints all arguments it recieves
  * @argc: number of arguments passed to the function
  * @argv: argument vector of pointers to strings
  *
- * Return: 0 if no errors, else 1
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	int a, b, c;
+	int i;
 
-	if (argc != 3)
+	for (i = 0; i < argc; i++)
 	{
-		puts("Error");
-		return (1);
+		printf("%s\n", argv[i]);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	c = a * b;
-	printf("%d\n", c);
 	return (0);
 }
+© 2022 GitHub, Inc.
